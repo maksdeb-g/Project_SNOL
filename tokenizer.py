1eb98dc51ff388e550fe2b23e4bd07695a6776fa
ItsMrMatematiko <janndavecasquejo@gmail.com>
2025-05-14 12:52:15 +0800

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,5 @@
+import evaluator as evaluator
+
 # check_by_precedence() function checks the order of the operator
 def get_precedence (operator):
     if operator == '+' or operator == '-':
@@ -12,6 +14,17 @@ def get_precedence (operator):
     Returns the postfix expression as a string
 '''
 
+# Checks if a specific character on the string is a valid operator - Jann Dave
+# Valid operators are +, -, *, /, and % (Addition, Subtraction, Multiplication, Division, and Modulo)
+def is_Operator(character):
+    match (character):	    
+        case '+': return true
+        case '-': return true
+        case '*': return true
+        case '/': return true
+        case '%': return true
+        case _: return false
+
 # Checks if the string is a valid variable name based on SNOL's rules: - Roslyn
 # Starts with a letter, followed by letters or digits.
 def is_variable(s: str) -> bool:
@@ -21,8 +34,6 @@ def is_variable(s: str) -> bool:
 # Allows negative sign and optional decimal part.
 def is_digit(s: str) -> bool:
     return re.fullmatch(r"-?\d+(\.\d+)?", s) is not None
-
-
 
 def conversion_helper(infix: str) -> str:
 
@@ -109,3 +120,36 @@ def conversion_helper(infix: str) -> str:
         postfix.append(mystack.pop())  
 
     return ' '.join(postfix)  # Join list into postfix string
+
+
+# Converts an Infix Expression into a Postfix Expression - Jann Dave
+# Uses 'evaluator.py' for helper functions
+def postfix_conversion(expr):
+    # Convert infix to postfix
+    postfix = conversion_helper(expr)
+
+    # Check for data type consistency
+    if not evaluator.error_finder(postfix):
+        print("SNOL> Error! Operands must be of the same data type in an arithmetic operation!")
+        return
+
+    else: 
+        # Determine expression data type and evaluate the postfix expression
+        if evaluator.expression_data_type(postfix):  # For integer data type in postfix expression
+            result = evaluator.evaluate_int_exp(postfix)
+
+            # Error Handling for non-integer types in Modulo Operation
+            if result == "ERROR":
+                print("SNOL> Error! Operands must be int type to perform Modulo!")
+                return
+
+        else:  # For float data type in postfix expression
+            result = evaluator.evaluate_float_exp(postfix)
+
+            # Error Handling for non-integer types in Modulo Operation
+            if result == "ERROR":
+                print("SNOL> Error! Operands must be int type to perform Modulo!")
+                return
+
+        # Terminate function operations
+        return
